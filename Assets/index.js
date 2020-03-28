3f1df1f0e5fa496237d6e49a88601cda42562a9e
Tabeed-H <50667297+Tabeed-H@users.noreply.github.com>
2020-03-28 13:16:21 +0530

    Added Questions

--- Assets/index.js
+++ Assets/index.js
@@ -1,2 +1,10 @@
-//checks for script
-console.log('script loaded');
+let currentpage = 0;
+let myQuiz = [
+    ['RAM Stands For:','Real Avialable Memory','Read Acess Memory','Random Access Memory','Random Available Memory',3],
+    ['RIP Stands For :','Rest In Peace','Ripped In Pieces','Rest In Park','Really Intelligent Person', 1]
+];
+document.getElementById("btnPrevious").addEventListener("click",moveBack);
+document.getElementById("btnNext").addEventListener("click",moveNext);
+let myQuestion = document.getElementById("questions");
+let myHeader = document.getElementById("quizHeader");
+let className = document.getElementsByClassName("answer");
