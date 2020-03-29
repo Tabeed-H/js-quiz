aba323f1a46600461ebc44b57b8ee1df131d2e6b
Tabeed-H <50667297+Tabeed-H@users.noreply.github.com>
2020-03-29 13:00:17 +0530

    added more functionality

--- Assets/main.js
+++ Assets/main.js
@@ -1,2 +1,61 @@
 let start = document.getElementsByClassName("start");
-let x = 0;
+let quiz = document.getElementById("quizContainer");
+let Question = document.getElementById("questions");
+let counter = document.getElementById("counter");
+let choiceA = document.getElementById("A");
+let choiceB = document.getElementById("B");
+let choiceC = document.getElementById("C");
+let choiceD = document.getElementById("D");
+let score = document.getElementById("scoreContainer");
+
+let questionArray = [
+    {
+        question : "HTML stands for",
+        choiceA : "Hyper text markup language",
+        choiceB : "Hyper team markup language",
+        choiceC : "Hyper text markeup language",
+        choiceD : "Hyper text markup land",
+        correct : "A",
+    },
+    {
+        question : "HTML stands for 2",
+        choiceA : "Hyper text markup language",
+        choiceB : "Hyper team markup language",
+        choiceC : "Hyper text markeup language",
+        choiceD : "Hyper text markup land",
+        correct : "A",
+    },
+    {
+        question : "HTML stands for 3",
+        choiceA : "Hyper text markup language",
+        choiceB : "Hyper team markup language",
+        choiceC : "Hyper text markeup language",
+        choiceD : "Hyper text markup land",
+        correct : "A",
+    }
+];
+
+
+let lastQuestionIndex = questionArray.length-1;
+let runningQuestionIndex = 0;
+
+function renderQuestion(){
+    let q = questionArray[runningQuestionIndex];
+    Question.innerHTML = q.question;
+    choiceA.innerHTML = q.choiceA;
+    choiceB.innerHTML = q.choiceB;
+    choiceC.innerHTML = q.choiceC;
+    choiceD.innerHTML = q.choiceD;
+};
+window.addEventListener("load", renderQuestion);
+let scores = 0;
+function checkAnswer(answer){
+    if( questionArray[runningQuestionIndex].correct  === answer){
+        runningQuestionIndex++;
+        scores++
+        document.getElementById("scoreObtained").innerHTML = scores;
+        renderQuestion();
+    }else{
+        alert("wrong")
+    }
+}
